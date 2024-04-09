3752a3bc2fd3749602e72d44f4030398a3d0fd97
sofija5 <sk20200313@student.fon.bg.ac.rs>
2024-04-09 22:53:07 +0200

    dodat kontroler za autentifikaciju i rute

--- laravelprojekat/routes/api.php
+++ laravelprojekat/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\DepartmentController;
 use App\Http\Controllers\EventTypeController;
 use Illuminate\Http\Request;
@@ -15,32 +16,45 @@ use App\Http\Controllers\EventController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/event-types', [EventTypeController::class, 'index']);
-Route::get('/event-types/{id}', [EventTypeController::class, 'show']);
-Route::post('/event-types', [EventTypeController::class, 'store']);
-Route::put('/event-types/{id}', [EventTypeController::class, 'update']);
-Route::delete('/event-types/{id}', [EventTypeController::class, 'destroy']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+ 
 Route::get('/departments', [DepartmentController::class, 'index']);
 Route::get('/departments/{id}', [DepartmentController::class, 'show']);
-Route::post('/departments', [DepartmentController::class, 'store']);
-Route::put('/departments/{id}', [DepartmentController::class, 'update']);
-Route::delete('/departments/{id}', [DepartmentController::class, 'destroy']);
 
-
-Route::get('/events', [EventController::class, 'index']);
-Route::get('/events/{id}', [EventController::class, 'show']);
-Route::post('/events', [EventController::class, 'store']);
-Route::put('/events/{id}', [EventController::class, 'update']);
-Route::delete('/events/{id}', [EventController::class, 'destroy']);
-
+ 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
  
 
 Route::middleware('auth:sanctum')->group(function () {
-     
 
-    // API ruta za izvoz događaja ulogovanog korisnika u .ics fajl
-    Route::get('/events/export', [EventController::class, 'exportToICS'])->name('events.export');
+    //KATEDRE
+    Route::post('/departments', [DepartmentController::class, 'store']);
+    Route::put('/departments/{id}', [DepartmentController::class, 'update']);
+    Route::delete('/departments/{id}', [DepartmentController::class, 'destroy']);
+
+    //DOGADJAJI
+    Route::get('/events', [EventController::class, 'index']);
+    Route::get('/events/{id}', [EventController::class, 'show']);
+    Route::post('/events', [EventController::class, 'store']);
+    Route::put('/events/{id}', [EventController::class, 'update']);
+    Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+        // API ruta za izvoz događaja ulogovanog korisnika u .ics fajl
+        Route::get('/events/export', [EventController::class, 'exportToICS'])->name('events.export');
+
+
+
+    //TIPOVI DOGADJAJA
+    Route::get('/event-types', [EventTypeController::class, 'index']);
+    Route::get('/event-types/{id}', [EventTypeController::class, 'show']);
+    Route::post('/event-types', [EventTypeController::class, 'store']);
+    Route::put('/event-types/{id}', [EventTypeController::class, 'update']);
+    Route::delete('/event-types/{id}', [EventTypeController::class, 'destroy']);
+
+   
 });
