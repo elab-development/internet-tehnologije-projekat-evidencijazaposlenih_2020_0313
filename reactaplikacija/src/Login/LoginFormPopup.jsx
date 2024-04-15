38e544d887ef2a54ce4ce9a30327ea27e95e69aa
ninamaricic <nm20190015@student.fon.bg.ac.rs>
2024-04-15 23:19:12 +0200

    omogucen login i register i logout

--- reactaplikacija/src/Login/LoginFormPopup.jsx
+++ reactaplikacija/src/Login/LoginFormPopup.jsx
@@ -1,12 +1,61 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './LoginFormPopup.css';  
+import { useNavigate } from 'react-router-dom';
 
-const LoginFormPopup = () => {
+const LoginFormPopup = ({setToken}) => {
+    let navigate=useNavigate();
     const [activeTab, setActiveTab] = useState('login');
+    const [firstName, setFirstName] = useState('Janko');
+    const [lastName, setLastName] = useState('Jankovic');
+    const [email, setEmail] = useState('nm20190015@student.fon.bg.ac.rs');
+    const [password, setPassword] = useState('password');
+    const [confirmPassword, setConfirmPassword] = useState('password');
+    const [departmentId, setDepartmentId] = useState('1');
 
     const switchTab = (tab) => {
         setActiveTab(tab);
     }; 
+
+    const handleLoginSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/login', {
+                email,
+                password
+            });
+            console.log('Login response:', response.data);
+            setToken(response.data.token)
+            sessionStorage.setItem("token",response.data.token)
+            navigate('/dogadjaji')
+        } catch (error) {
+            console.error('Error logging in:', error);
+            // Dodaj logiku za obradu greške pri logovanju
+        }
+    };
+
+    const handleRegisterSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/register', {
+                firstName,
+                lastName,
+                email,
+                password,
+                confirm_password: confirmPassword,
+                department_id: departmentId
+            });
+            console.log('Register response:', response.data);
+            console.log('Login response:', response.data);
+            setToken(response.data.token)
+            sessionStorage.setItem("token",response.data.token)
+            navigate('/dogadjaji')
+        } catch (error) {
+            console.error('Error registering:', error);
+            // Dodaj logiku za obradu greške pri registraciji
+        }
+    };
+
     return (
         <div> 
             <section>
@@ -18,9 +67,9 @@ const LoginFormPopup = () => {
                                     <input className="radio" id="tab-1" name="tabs-name" type="radio" checked={activeTab === 'login'} onChange={() => switchTab('login')}/>
                                     <label htmlFor="tab-1" className="table"><span>Login</span></label>
                                     <div className="tabs-content" style={{ display: activeTab === 'login' ? 'block' : 'none' }}>
-                                        <form action="">
-                                            <input type="email" placeholder="Email" required/>
-                                            <input type="password" placeholder="Password" required/>
+                                        <form onSubmit={handleLoginSubmit}>
+                                            <input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                            <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
                                             <input type="submit" value="Log In"/>
                                         </form>
                                         <form className="forgot-password" action="">
@@ -36,10 +85,13 @@ const LoginFormPopup = () => {
                                     <input className="radio" id="tab-2" name="tabs-name" type="radio" checked={activeTab === 'signup'} onChange={() => switchTab('signup')}/>
                                     <label htmlFor="tab-2" className="table"><span>Sign up</span></label>
                                     <div className="tabs-content" style={{ display: activeTab === 'signup' ? 'block' : 'none' }}>
-                                        <form action="">
-                                            <input type="email" placeholder="Email" required/>
-                                            <input type="password" placeholder="Password" required/>
-                                            <input type="password" placeholder="Confirm password" required/>
+                                        <form onSubmit={handleRegisterSubmit}>
+                                            <input type="text" placeholder="First Name" required value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                                            <input type="text" placeholder="Last Name" required value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                                            <input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
+                                            <input type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
+                                            <input type="password" placeholder="Confirm password" required value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}/>
+                                            <input type="text" placeholder="Department ID" required value={departmentId} onChange={(e) => setDepartmentId(e.target.value)}/>
                                             <input type="submit" value="Sign Up"/>
                                         </form>
                                     </div>
