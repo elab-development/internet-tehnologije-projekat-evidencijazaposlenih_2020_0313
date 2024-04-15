ca16d28e6d485a93be33d7983f3fc977fef7f1a9
ninamaricic <nm20190015@student.fon.bg.ac.rs>
2024-04-15 23:12:44 +0200

    kreirana reusable komponenta navbar

--- reactaplikacija/src/App.js
+++ reactaplikacija/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginFormPopup from './Login/LoginFormPopup';
 import './App.css';
 import HomePage from './Home/HomePage';
+import Navbar from './Reusable/Navbar';
 
 function App() {
+  const [token,setToken]=useState(null);
   return (
     <Router>
       <div>
+        <Navbar token={token} setToken={setToken}></Navbar>
         <Routes>
         <Route path="/" element={<HomePage />} />
-          <Route path="/loginregister" element={<LoginFormPopup />} />
+          <Route path="/loginregister" element={<LoginFormPopup setToken={setToken} />} />
         </Routes>
       </div>
     </Router>
---
+++ reactaplikacija/src/Reusable/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link,  useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './Navbar.css'
+function Navbar({ token, setToken }) {
+  const history = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post('http://127.0.0.1:8000/api/logout', null, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+     
+      setToken(null);
+      history('/loginregister');
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar-item">Pocetna</Link>
+      {token ? (
+        <button onClick={handleLogout} className="navbar-item">Logout</button>
+      ) : (
+        <Link to="/loginregister" className="navbar-item">Prijava</Link>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
